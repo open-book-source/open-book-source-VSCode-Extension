805281da98da82b4f2bee05e1c50d52f5dce174f
sososdk <csshuai2009@gmail.com>
2024-03-01 10:47:09 +0800

    优化模版

--- assets/template/repository_generator.js
+++ assets/template/repository_generator.js
@@ -5,32 +5,32 @@ const script = require('./src/script');
 const dir = './sources';
 
 const sourceFiles = fs.readdirSync(dir).map(filename => {
-    return path.join(dir, filename);
+  return path.join(dir, filename);
 });
 
 const sources = [];
 for (const file of sourceFiles) {
-    if (!fs.statSync(file).isFile) continue;
-    const meta = script.parseMeta(fs.readFileSync(file, 'utf8'));
-    if (meta.status === 'obsolete') continue;
+  if (!fs.statSync(file).isFile) continue;
+  const meta = script.parseMeta(fs.readFileSync(file, 'utf8'));
+  if (meta.status === 'obsolete') continue;
 
-    sources.push({
-        format: 'javascript',
-        type: meta.type,
-        name: meta.name,
-        locale: meta.locale,
-        domain: meta.domain,
-        homepage: meta.homepage,
-        icon: meta.icon,
-        description: meta.description,
-        version: meta.version,
-        changelog: meta.changelog,
-    });
+  sources.push({
+    format: 'javascript',
+    type: meta.type,
+    name: meta.name,
+    locale: meta.locale,
+    domain: meta.domain,
+    homepage: meta.homepage,
+    icon: meta.icon,
+    description: meta.description,
+    version: meta.version,
+    changelog: meta.changelog,
+  });
 }
 const repository = {
-    author: '[Author]',
-    homepage: 'https://github.com/[user]/[repo]',
-    sources: sources,
+  author: '[Author]',
+  homepage: 'https://github.com/[user]/[repo]',
+  sources: sources,
 };
 
 fs.writeFileSync('./repository.json', JSON.stringify(repository, undefined, 2));
